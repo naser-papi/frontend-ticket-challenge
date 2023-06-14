0eef0ed08079bb91bb88d0c854d00a28b3cc1f84
papinaser <papinaser@gmail.com>
2023-06-14 22:10:24 +0330

    improve the performance of rendering seats smoothly

--- src/SeatList.tsx
+++ src/SeatList.tsx
@@ -9,6 +9,7 @@ const SeatList = () => {
   const { mapId } = useParams();
   const [seats, setSeats] = useState([]);
   const [lastSeen, setLastSeen] = useState(1);
+  const [elements, setElements] = useState([]);
   const [callApi] = useCallApi();
 
   useEffect(() => {
@@ -25,6 +26,38 @@ const SeatList = () => {
     //   .map(() => [rnd(), rnd(), rnd(), rnd()]);
     // setSeats(sl);
   }, []);
+
+  useEffect(() => {
+    const page = Math.floor(lastSeen / PAGE_COUNT) + 1;
+    console.log("page", page, lastSeen);
+    let limit = page * PAGE_COUNT;
+    if (limit > seats.length) {
+      limit = seats.length + 1;
+    }
+    const list = seats.slice(elements.length, limit).map((row: [], rowIndex, rows) => {
+      rowIndex++;
+      return (
+        <ul key={"p" + page + "row" + rowIndex} className={"columnsContainer"}>
+          {row.map((col, colIndex, cols) => {
+            colIndex++;
+            const isLast = rowIndex === rows.length && colIndex === cols.length;
+            return (
+              <Seat
+                selectHandler={selectSeatHandler}
+                rowIndex={rowIndex}
+                colIndex={colIndex}
+                lastSeen={lastSeen}
+                setLastSeen={setLastSeen}
+                isLast={isLast}
+                isReserve={seats[rowIndex - 1][colIndex - 1] != 0}
+              />
+            );
+          })}
+        </ul>
+      );
+    });
+    setElements([...elements, ...list]);
+  }, [lastSeen, seats.length]);
   const selectSeatHandler = (seat: ISeat) => {
     callApi({
       method: "POST",
@@ -40,38 +73,9 @@ const SeatList = () => {
     });
   };
 
-  const page = Math.floor(lastSeen / PAGE_COUNT) + 1;
-  console.log("page", page, lastSeen);
-  let limit = page * PAGE_COUNT;
-  if (limit > seats.length) {
-    limit = seats.length + 1;
-  }
-  const list = seats.slice(0, limit).map((row: [], rowIndex, rows) => {
-    rowIndex++;
-    return (
-      <ul key={"p" + page + "row" + rowIndex} className={"columnsContainer"}>
-        {row.map((col, colIndex, cols) => {
-          colIndex++;
-          const isLast = rowIndex === rows.length && colIndex === cols.length;
-          return (
-            <Seat
-              selectHandler={selectSeatHandler}
-              rowIndex={rowIndex}
-              colIndex={colIndex}
-              lastSeen={lastSeen}
-              setLastSeen={setLastSeen}
-              isLast={isLast}
-              isReserve={seats[rowIndex - 1][colIndex - 1] != 0}
-            />
-          );
-        })}
-      </ul>
-    );
-  });
-
   return (
     <section className={"listContainer"}>
-      {list.length ? list : <p>please wait for loading map seats</p>}
+      {elements.length ? elements : <p>please wait for loading map seats</p>}
     </section>
   );
 };
